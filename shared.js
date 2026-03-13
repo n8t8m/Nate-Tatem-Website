7a234832c2a107d98363b355c9e3f65c2d2eee99
n8t8m <natem262@gmail.com>
2026-03-13 09:52:50 -0700

    Add files via upload

--- shared.js
+++ shared.js
@@ -1,6 +1,6 @@
 // Configuration object - single source of truth for links and data
 const config = {
   links: {
-    resume: 'https://docs.google.com/document/d/e/2PACX-1vRY0ZIeg2jjJcM-JYgyDhLIzIyQTT1-OSwIYwJOUCFVk4jvfrsEpyhzXNUwHYXTSdS3HST92mAI8Khm/pub'
+    resume: 'https://docs.google.com/document/d/1RovgvD9X_7BbHX_mhOtFrE-JuVj5L-khV3Grfie6J5M/edit?usp=sharing'
   }
 };
