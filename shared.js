881bbb77cda5d59f9394cd92fd62cc729aaa667a
n8t8m <natem262@gmail.com>
2026-03-13 09:22:56 -0700

    Add shared.js configuration file

---
+++ shared.js
@@ -0,0 +1,6 @@
+// Configuration object - single source of truth for links and data
+const config = {
+  links: {
+    resume: 'https://docs.google.com/document/d/e/2PACX-1vRY0ZIeg2jjJcM-JYgyDhLIzIyQTT1-OSwIYwJOUCFVk4jvfrsEpyhzXNUwHYXTSdS3HST92mAI8Khm/pub'
+  }
+};
